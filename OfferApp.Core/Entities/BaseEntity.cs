e4c8603b22b796181ca3ab75fb9c0601042048e8
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-07-02 17:28:11 +0200

    [Module6] Task 1 Solution

--- OfferApp.Core/Entities/BaseEntity.cs
+++ OfferApp.Core/Entities/BaseEntity.cs
@@ -1,8 +1,13 @@
-﻿namespace OfferApp.Core.Entities
+﻿using System.Runtime.Serialization;
+
+namespace OfferApp.Core.Entities
 {
+    [DataContract] // atrybut niezbędny do serializaci i deserializacji prywatnych właściwości
     public abstract class BaseEntity
     {
-        public int Id { get; }
+        [DataMember] // atrybut niezbędny do serializaci i deserializacji prywatnych właściwości
+        // wystąpił problem z readonly właściwością i z tego powodu aby nie komplikować ustawiłem private set
+        public int Id { get; private set; }
 
         public BaseEntity(int id)
         {
