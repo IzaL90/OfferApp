8f78294832d9ab311e3eff2d286fc421bf12a2b2
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-07-02 19:25:19 +0200

    [Module6] Task 2 Solution

---
+++ OfferApp.Infrastructure/Cache/CacheWrapper.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.Caching;
+
+namespace OfferApp.Infrastructure.Cache
+{
+    // MemoryCache nie dostarcza interfejsu i z tego powodu zastosowałem wrapper.
+    // Dodatkowo MemoryCache obudowuje każdy element jako CacheItem.
+    // MemoryCache do każdej wartości podochodzi indywidualnie tzn każdemu obiektowi można ustawić własne jego policy.
+    // My akurat potrzebujemy globalnych ustawień dlatego przekazałem CacheOptions w konstruktorze
+    internal sealed class CacheWrapper : ICacheWrapper
+    {
+        private readonly MemoryCache _cache;
+        private readonly CacheOptions _options;
+
+        public CacheWrapper(CacheOptions cacheOptions)
+        {
+            _cache = new MemoryCache("OfferApp");
+            _options = cacheOptions;
+        }
+
+        public T Add<T>(string key, T item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            _cache.Add(new CacheItem(key, item), new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.Add(_options.CacheEntryExpired)
+            });
+            return item;
+        }
+
+        public void Delete(string key)
+        {
+            if (_cache.Get(key) is not null)
+            {
+                _cache.Remove(key);
+            }
+        }
+
+        public T? Get<T>(string key)
+        {
+            return (T?) _cache.Get(key);
+        }
+
+        public T Update<T>(string key, T item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            if (_cache.Get(key) is not null)
+            {
+                _cache.Remove(key);
+            }
+
+            _cache.Add(new CacheItem(key, item), new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.Add(_options.CacheEntryExpired)
+            });
+            return item;
+        }
+    }
+}
--- OfferApp.Infrastructure/Extensions.cs
+++ OfferApp.Infrastructure/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using OfferApp.Core.Entities;
 using OfferApp.Core.Repositories;
+using OfferApp.Infrastructure.Cache;
 using OfferApp.Infrastructure.Repositories;
 
 namespace OfferApp.Infrastructure
@@ -10,7 +12,24 @@ namespace OfferApp.Infrastructure
         {
             //return services.AddSingleton(typeof(IRepository<>), typeof(Repository<>));
             // podmiana na repozytorium w pliku xml
-            return services.AddScoped(typeof(IRepository<>), typeof(XmlRepository<>));
+            services.AddScoped(typeof(IRepository<>), typeof(XmlRepository<>))
+                // rejestracja jako scoped będzie przydatna w przypadku udekorowania repozytorium używającego cache
+                .AddScoped(typeof(XmlRepository<>))
+                .AddSingleton<ICacheWrapper, CacheWrapper>(_ => new CacheWrapper(new CacheOptions
+                {
+                    CacheEntryExpired = TimeSpan.FromMinutes(5),
+                }));
+
+            // rejestracja repozytorium które posiada cache
+            // można usprawnić to rozwiązanie bazując np na atrybutach i poprzez refleksję odpowiednio rejestrować
+            // jednak w tym przypadku wystraczy taka prosta rejesteracja
+            services.AddScoped<IRepository<Bid>>(sp =>
+            {
+                var repository = sp.GetRequiredService<XmlRepository<Bid>>();
+                var cacheWrapper = sp.GetRequiredService<ICacheWrapper>();
+                return new CacheRepository<Bid>(repository, cacheWrapper);
+            });
+            return services;
         }
     }
 }
