302b8c1b6e0046de15da76272a35b3893c563cfe
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-07-04 20:58:16 +0200

    [Module7] Task 3 Solution
    Added logging

--- OfferApp.Infrastructure/Database/Extensions.cs
+++ OfferApp.Infrastructure/Database/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OfferApp.Infrastructure.Repositories;
+using OfferApp.Migrations;
 
 namespace OfferApp.Infrastructure.Database
 {
@@ -8,9 +9,36 @@ namespace OfferApp.Infrastructure.Database
     {
         public static IServiceCollection AddDatabase(this IServiceCollection services)
         {
+            services.AddEFCore();
+            return services;
+        }
+
+        public static IServiceCollection AddDbContext(this IServiceCollection services)
+        {
             var databaseOptions = services.GetService<DatabaseOptions>();
             var serverVersion = ServerVersion.AutoDetect(databaseOptions.ConnectionString);
             services.AddDbContext<OfferDbContext>(options => options.UseMySql(databaseOptions.ConnectionString, serverVersion));
+            return services;
+        }
+
+        internal static IServiceCollection AddFileStoreProvider(this IServiceCollection services)
+        {
+            services.AddFileRepositories();
+            return services;
+        }
+
+        internal static IServiceCollection AddDapper(this IServiceCollection services)
+        {
+            services.AddDapperRepositories();
+            services.AddTransient<IDbInitializer, DapperDbInitializer>();
+            var databaseOptions = services.GetService<DatabaseOptions>();
+            services.AddMigrations(databaseOptions.ConnectionString!);
+            return services;
+        }
+
+        internal static IServiceCollection AddEFCore(this IServiceCollection services)
+        {
+            services.AddDbContext();
             services.AddEFCoreRepositories();
             services.AddTransient<IDbInitializer, EFDbInitializer>();
             return services;
