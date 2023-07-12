e9176a5c3c42290a3aa61ee64be994d8f6bf3317
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-07-13 01:02:48 +0200

    [Module9] Task 2 Solution

---
+++ OfferApp.IntegrationTests/Common/Fixtures.cs
@@ -0,0 +1,19 @@
+﻿using OfferApp.Core.Entities;
+
+namespace OfferApp.IntegrationTests.Common
+{
+    internal static class Fixtures
+    {
+        public static Bid CreateBid()
+        {
+            return Bid.Create("Name#1", "Description12345", 100);
+        }
+
+        public static Bid CreatePublishedBid()
+        {
+            var bid = CreateBid();
+            bid.Publish();
+            return bid;
+        }
+    }
+}
---
+++ OfferApp.IntegrationTests/Common/SeedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using OfferApp.Core.Entities;
+using OfferApp.Infrastructure.Database;
+
+namespace OfferApp.IntegrationTests.Common
+{
+    internal class SeedData : IHostedService
+    {
+        private readonly OfferDbContext _dbContext;
+
+        public SeedData(OfferDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _dbContext.Bids.Add(Bid.Create("Bid#Name#1", "Bid#Description#1", 10));
+            _dbContext.Bids.Add(Bid.Create("Bid#Name#2", "Bid#Description#2", 20));
+            _dbContext.Bids.Add(Bid.Create("Bid#Name#3", "Bid#Description#3", 30));
+            _dbContext.Bids.Add(Bid.Create("Bid#Name#4", "Bid#Description#4", 40));
+            _dbContext.Bids.Add(Bid.Create("Bid#Name#5", "Bid#Description#5", 50));
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
