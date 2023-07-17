a44677df7a70765fd9a865906c61873062df2dae
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2023-07-17 19:31:47 +0200

    [Module10] Task 3 Solution

--- OfferApp.UI/Program.cs
+++ OfferApp.UI/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("backend") ?? throw new InvalidOperationException("Backend url should be provided")) });
 builder.Services.AddServices();
 
 await builder.Build().RunAsync();
